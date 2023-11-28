9a8f3242e14c9953fa793f85a61181064e994a12
lasha1008 <lasha1008@gmail.com>
2023-11-29 01:42:38 +0400

    DTOs added.

---
+++ UserManagement.DTO/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.DTO;
+
+public class User
+{
+    [Key]
+    public int UserId { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Username { get; set; } = null!;
+
+    [Required]
+    [MaxLength(64)]
+    public string Password { get; set; } = null!;
+
+    [Required]
+    [MaxLength(50)]
+    public string Email { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+
+    public UserProfile? UserProfile { get; set; }
+}
---
+++ UserManagement.DTO/UserProfile.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserManagement.DTO;
+
+public class UserProfile
+{
+    [Key, ForeignKey("User")]
+    public int UserId { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    [MaxLength(50)]
+    public string LastName { get; set; } = null!;
+
+    [Required]
+    [MaxLength(11)]
+    public string PersonalNumber { get; set; } = null!;
+
+
+    [Required]
+    public User User { get; set; } = null!;
+}
